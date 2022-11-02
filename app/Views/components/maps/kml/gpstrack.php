47ddb586f8453208a918e29dec21092e76674a6b
Rein Velt <rein@velt.org>
2022-11-02 15:26:29 +0100

    error fix

--- app/Views/components/maps/kml/gpstrack.php
+++ app/Views/components/maps/kml/gpstrack.php
@@ -1,5 +1,5 @@
 <?php header("Access-Control-Allow-Origin: *"); ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?php print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
     <?php
     foreach($data as $row)
