c78354d5c359d928ae01288d5e114321abba41d2
Rein Velt <rein@velt.org>
2022-11-17 14:02:16 +0100

    dx

--- public/js/data/mission.js
+++ public/js/data/mission.js
@@ -155,9 +155,9 @@ function updateMissiontargets(data)
               html=html+'   <div class="mb-1 title"><img src="http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png" style="width:25px; float:left;"><a href="#'+data[i].id+'">'+data[i].name+'</a></div>';
               if (oldId>0)
               {
-                html=html+'   <a  style="font-size:18px; " class="material-icons hover" onclick="getRoute('+data[i].oldId+','+data[i].id+'); return false;">directions</a>';
+                html=html+ '<a  style="font-size:18px; " onclick="getRoute('+data[i].oldId+','+data[i].id+'); return false;"><span class="material-icons hover" >directions</span></a>';
               }
-              html=html+'   <a data-bs-toggle="modal" data-bs-target="#formModalMissionTarget" style="font-size:18px; float:right;" class="material-icons hover" onclick="getMissiontargetForm('+data[i].missionId+','+data[i].id+'); return false;">settings</a>';
+              html=html+'   <a data-bs-toggle="modal" data-bs-target="#formModalMissionTarget" style="font-size:18px; float:right;" onclick="getMissiontargetForm('+data[i].missionId+','+data[i].id+'); return false;">s<span class="material-icons hover" >settings</span></a>';
               html=html+' </div>';
               html=html+'<div class="missioninfo">'
               html=html+  '<div class="subtitle">';
