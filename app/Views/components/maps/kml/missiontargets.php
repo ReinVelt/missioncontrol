8161b435cddfb01470287697ef3120149eaa0d0c
Rein Velt <rein@velt.org>
2022-11-17 10:10:20 +0100

    d

--- app/Views/components/maps/kml/missiontargets.php
+++ app/Views/components/maps/kml/missiontargets.php
@@ -65,7 +65,7 @@ foreach($gpslog as $row)
     {
         $dist=0;
     }
-        if ($dist<0.3 && isset($oldRow))
+        if ($dist<1 && isset($oldRow))
         {
             ?>
            <Placemark id="gpslogroute<?= $oldRow["id"]."-".$row["id"]; ?>">
