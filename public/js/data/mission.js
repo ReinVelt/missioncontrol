81143adfe5ada36a255cbe863c502ad6eb0f8575
Rein Velt <rein@velt.org>
2022-11-18 00:44:31 +0100

    as

--- public/js/data/mission.js
+++ public/js/data/mission.js
@@ -97,8 +97,8 @@ function updateRoutes(data)
 {
 
   const image = new ol.style.Circle({
-    radius: 3,
-    fill: null,
+    radius: 2,
+    fill:  new ol.style.Fill({ color: 'rgba(128,0,0,0.2)',}),
     stroke: new ol.style.Stroke({color: 'blue', width: 1}),
   });
   
