f271daef2d217d2457b7ade7cc0668e1000ef1da
Rein Velt <rein@velt.org>
2022-11-17 09:37:08 +0100

    d

--- app/Views/components/maps/kml/missiontargets.php
+++ app/Views/components/maps/kml/missiontargets.php
@@ -62,7 +62,7 @@ foreach($gpslog as $row)
                 <description>[<?= $row["datum"]; ?>] <?= $row["description"]; ?></description>
                 <style>
                      <LineStyle>
-                            <color>ffffff00</color>
+                            <color>33ffff00</color>
                     </LineStyle>
                 </style>
                 <LineString>
