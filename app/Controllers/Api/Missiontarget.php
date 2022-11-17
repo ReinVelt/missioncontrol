d7774d9fea612969dc41bbaa1e424fc4f0902a74
Rein Velt <rein@velt.org>
2022-11-17 13:38:54 +0100

    fx

--- app/Controllers/Api/Missiontarget.php
+++ app/Controllers/Api/Missiontarget.php
@@ -148,7 +148,7 @@ class Missiontarget extends ResourceController
         $destinationll=$destination["longitude"].",".$destination["latitude"];
         $routeModel=new OpenrouteserviceModel();
         $route=$routeModel->getRoute($originll,$destinationll);
-        $response = json_encode($route);
+        $response = json_decode($route);
         
         return $this->respond($response);
     }
