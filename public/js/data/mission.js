c4a168693190bed806c230c7ae4403759209920b
Rein Velt <rein@velt.org>
2022-11-17 16:20:18 +0100

    fx

--- public/js/data/mission.js
+++ public/js/data/mission.js
@@ -96,75 +96,17 @@ function getRoute(orig,dest)
 function updateRoutes(data)
 {
 
-  const image = new CircleStyle({
+  const image = new ol.style.Circle({
     radius: 5,
     fill: null,
-    stroke: new Stroke({color: 'red', width: 1}),
+    stroke: new ol.style.Stroke({color: 'red', width: 1}),
   });
   
   const styles = {
-    'Point': new Style({
+    'Point': new ol.Style({
       image: image,
     }),
-    'LineString': new Style({
-      stroke: new Stroke({
-        color: 'green',
-        width: 1,
-      }),
-    }),
-    'MultiLineString': new Style({
-      stroke: new Stroke({
-        color: 'green',
-        width: 1,
-      }),
-    }),
-    'MultiPoint': new Style({
-      image: image,
-    }),
-    'MultiPolygon': new Style({
-      stroke: new Stroke({
-        color: 'yellow',
-        width: 1,
-      }),
-      fill: new Fill({
-        color: 'rgba(255, 255, 0, 0.1)',
-      }),
-    }),
-    'Polygon': new Style({
-      stroke: new Stroke({
-        color: 'blue',
-        lineDash: [4],
-        width: 3,
-      }),
-      fill: new Fill({
-        color: 'rgba(0, 0, 255, 0.1)',
-      }),
-    }),
-    'GeometryCollection': new Style({
-      stroke: new Stroke({
-        color: 'magenta',
-        width: 2,
-      }),
-      fill: new Fill({
-        color: 'magenta',
-      }),
-      image: new CircleStyle({
-        radius: 10,
-        fill: null,
-        stroke: new Stroke({
-          color: 'magenta',
-        }),
-      }),
-    }),
-    'Circle': new Style({
-      stroke: new Stroke({
-        color: 'red',
-        width: 2,
-      }),
-      fill: new Fill({
-        color: 'rgba(255,0,0,0.2)',
-      }),
-    }),
+    
   };
   
   const styleFunction = function (feature) {
