f0a292fa7638399de9bb74dc9bfa9f214ad4fa3f
Rein Velt <rein@velt.org>
2022-11-18 23:40:01 +0100

    ss

--- app/Views/components/maps/kml/missiontargets.php
+++ app/Views/components/maps/kml/missiontargets.php
@@ -67,7 +67,7 @@ foreach($media as $row)
                         </Icon>
                     </IconStyle>
                     <LabelStyle>
-                    <scale>0.0001/scale>
+                    <scale>0.0001</scale>
                 </LabelStyle>
                 </Style>
                 <Point>
